993c280b05cd3706eb3297384477526cf19d9c98
Cem Cavus <45109328+c3mcavus@users.noreply.github.com>
2021-01-08 14:27:00 +0300

    Add files via upload

---
+++ Data Types and Memory Concepts/answers/answer3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ( void ){
+
+    printf("1 2 3 4\n"); // part a
+
+    printf("%d %d %d %d\n",1,2,3,4); // part b
+
+    printf("1 "); // part c
+    printf("2 ");
+    printf("3 ");
+    printf("4\n");
+}
